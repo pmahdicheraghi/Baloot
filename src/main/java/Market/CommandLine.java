ad3ce5a6ddf3b04b1c96518f83ca04ba54240057
iamalinaji <anm33918@gmail.com>
2023-02-28 20:35:25 +0330

    Refactored command processing in the project to use a switch statement for improved readability.

--- src/main/java/Market/CommandLine.java
+++ src/main/java/Market/CommandLine.java
@@ -17,26 +17,48 @@ public class CommandLine {
             int spaceIndex = inputCommand.indexOf(" ");
             String command = inputCommand.substring(0, spaceIndex);
             String json = inputCommand.substring(spaceIndex + 1);
+            JSONObject params = JsonParser.parseJson(json);
+            String dateString;
             try {
-                if (command.equals("addUser")) {
-                    JSONObject params = JsonParser.parseJson(json);
-                    String username = (String) params.get("username");
-                    String password = (String) params.get("password");
-                    String email = (String) params.get("email");
-                    String dateString = (String) params.get("birthDate");
-                    Date birthDate = dateFormat.parse(dateString);
-                    String address = (String) params.get("address");
-                    int credit = (int)(long) params.get("credit");
+                switch (command) {
+                    case "addUser":
+                        String username = (String) params.get("username");
+                        String password = (String) params.get("password");
+                        String email = (String) params.get("email");
+                        dateString = (String) params.get("birthDate");
+                        Date birthDate = dateFormat.parse(dateString);
+                        String address = (String) params.get("address");
+                        int credit = (int) (long) params.get("credit");
+                        market.addUser(username, password, email, birthDate, address, credit);
+                        printSuccess("addUser");
+                        break;
+                    case "addProvider":
+                        int id = (int)(long) params.get("id");
+                        String name = (String) params.get("name");
+                        dateString=(String) params.get("registryDate");
+                        Date registryDate = dateFormat.parse(dateString);
+                        market.addProvider(id,name,registryDate);
+                        printSuccess("addProvider");
+                    case "addCommodity":
+                    case "getCommoditiesList":
+                    case "rateCommodity":
+                    case "addToBuyList":
+                    case "removeFromBuyList":
+                    case "getCommodityById":
+                    case "getCommoditiesByCategory":
+                    case "getBuyList":
+                    default:
+                        System.out.println("{\"success\": false, \"data\": \"Invalid command\"}");
 
-                    market.addUser(username, password, email, birthDate, address, credit);
-                }
-                else {
-                    System.out.println("{\"success\": false, \"data\": \"Invalid command\"}");
                 }
             } catch (Exception e) {
                 System.out.println("{\"success\": false, \"data\": " + e.getMessage() + "}");
             }
 
         } while (!inputCommand.equals("exit"));
+    }
+    public static void printSuccess(String OperationName){
+        String message="Operation "+ OperationName + "done successfully";
+        System.out.printf("{\"success\": true, \"data\": \"%s\"}",message);
     }
 }
