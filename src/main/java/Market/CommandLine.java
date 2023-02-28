f0a0bc4583c9412db10ca73bff80d5e1808097fe
iamalinaji <anm33918@gmail.com>
2023-02-28 19:08:56 +0330

    Implemented a more robust method for handling user inputs in the project.

--- src/main/java/Market/CommandLine.java
+++ src/main/java/Market/CommandLine.java
@@ -1,7 +1,7 @@
 package Market;
 
 import org.json.simple.JSONObject;
-
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -9,26 +9,28 @@ public class CommandLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         MarketManager market = MarketManager.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String inputCommand;
         do {
             System.out.println(">>");
             inputCommand = scanner.nextLine();
-            String command = "addUser";
-            String json = " {\"username\": \"us%er1\", \"password\": \"1234\", \"email\": \"user@gmail.com\", \"birthDate\":\n" +
-                    "\"1977-09-15\", \"address\": \"address1\", \"credit\": 1500}";
-
+            int spaceIndex = inputCommand.indexOf(" ");
+            String command = inputCommand.substring(0, spaceIndex);
+            String json = inputCommand.substring(spaceIndex + 1);
             try {
-                if (command == "addUser") {
+                if (command.equals("addUser")) {
                     JSONObject params = JsonParser.parseJson(json);
                     String username = (String) params.get("username");
                     String password = (String) params.get("password");
                     String email = (String) params.get("email");
-                    Date birthDate = new Date();
+                    String dateString = (String) params.get("birthDate");
+                    Date birthDate = dateFormat.parse(dateString);
                     String address = (String) params.get("address");
                     int credit = (int)(long) params.get("credit");
 
                     market.addUser(username, password, email, birthDate, address, credit);
-                } else {
+                }
+                else {
                     System.out.println("{\"success\": false, \"data\": \"Invalid command\"}");
                 }
             } catch (Exception e) {
